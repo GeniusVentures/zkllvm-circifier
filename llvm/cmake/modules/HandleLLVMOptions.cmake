0821f0591c27c64393184cdf390d36a6881843af
Eduardo Menges Mattje <eduardomattje@gmail.com>
2024-11-25 17:47:55 -0300

    Fixed LLVM compiling on iOS

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -236,9 +236,9 @@ endif()
 
 # Pass -Wl,-z,defs. This makes sure all symbols are defined. Otherwise a DSO
 # build might work on ELF but fail on MachO/COFF.
-if(NOT (CMAKE_SYSTEM_NAME MATCHES "Darwin|FreeBSD|OpenBSD|DragonFly|AIX|OS390" OR
-        WIN32 OR CYGWIN) AND
-   NOT LLVM_USE_SANITIZER)
+if(NOT(CMAKE_SYSTEM_NAME MATCHES "Darwin|FreeBSD|OpenBSD|DragonFly|AIX|OS390|iOS" OR
+    WIN32 OR CYGWIN) AND
+    NOT LLVM_USE_SANITIZER)
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-z,defs")
 endif()
 
